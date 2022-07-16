6119c15e819d13148e62960d5181a131dc18357a
able <yarv.crypto@gmail.com>
2022-07-16 10:53:03 +0100

    Setup react application;

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './App.module.css'
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.header}>tracker</h1>
+    </div>
+  )
+};
+
+export default App;
