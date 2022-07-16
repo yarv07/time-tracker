4cecf378b73e23a9aad6ab2ac9e9b862ba81e0c0
able <yarv.crypto@gmail.com>
2022-07-16 22:20:39 +0100

    Add components to App;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Input from './components/Input'
+import TrackersList from './components/TrackersList'
 import styles from './App.module.css'
 
 const App = () => {
   return (
     <div className={styles.app}>
       <h1 className={styles.header}>tracker</h1>
+      <Input />
+      <TrackersList />
     </div>
   )
 };
