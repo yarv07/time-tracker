5933d3ca9c78994e0a3d9351d3526d5dafa43e9c
able <yarv.crypto@gmail.com>
2022-07-16 11:02:05 +0100

    Add redux;

---
+++ src/store/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import countersReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'counters',
+  storage: storage,
+}
+const persistedReducer = persistReducer(persistConfig, countersReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store)
+
+export { store, persistor };
